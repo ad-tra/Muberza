851947304fdd429cf19ea2e49a654ba1d654a710
Adam <aew31gcmuy@gmail.com>
2021-04-30 19:33:02 -0400

    refactored sass with @include

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,14 @@ import Logo from "../../data/svg/logo.svg"
 
 export default function Navbar(){ 
     const [state, setState] = useState(false)
-    function handleHam(){
+    function closeExpandHam(){
         setState(state => !state)
     }
     return (
         <nav className= "navbar">
             <div className = "navbar_controls">
                 
-                <button className = "burger" onClick={handleHam} >
+                <button className = "burger" onClick={closeExpandHam} >
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="143.7 162.1 139.9 92.8" width="30px" height="20px">
                         <path d="M144 232h140v23H144zM144 197h140v23H144zM144 162h140v23H144z"/>
                     </svg>
@@ -22,7 +22,7 @@ export default function Navbar(){
             </div>
 
             
-            <ul className={ "navbar_links ".concat(state ? "expanded" : "")}>
+            <ul className={ `navbar_links ${state ? "expanded" : ""}`}>
                 <li ><Link to="/404">شكـونـا</Link></li>
                 <li ><Link to="/404">اتصل بنا</Link></li>
                 <li ><Link to="/404">اعطينا رايك</Link></li>
