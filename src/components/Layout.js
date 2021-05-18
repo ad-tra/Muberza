1f8f7fcae8b541d4334e174a9216effcc7adf9ca
ad-tra <aew31gcmuy@gmail.com>
2021-05-17 23:20:41 -0400

    experm with templates through politicianPP

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
 
-export default function Layout({children}) {
+export default function Layout({className, children}) {
     return (
-        <div>
+        <div className= {className}>
             <Navbar />
             {children}
             <footer>Shot myself in the footer</footer>
