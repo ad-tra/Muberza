afa949f20c35adb4074679ad6597d2fc63fd6006
ad-tra <aew31gcmuy@gmail.com>
2021-06-05 02:20:06 -0400

    finished condensed viewership stats

--- scrapers/viewership_stats/main.js
+++ scrapers/viewership_stats/main.js
@@ -6,30 +6,50 @@ const fs = require("fs");
 const dayjs = require('dayjs')
 
 const {getStats} =  require("./evaluate.js");
-const mockData = JSON.parse("[{\"node\":{\"slug\":\"abir-moussi\",\"facebookSlug\":\"AbirMoussiOfficielle\",\"id\":\"58ce0cd4-65cf-5515-b0a8-83786a2a2ef7\",\"parent\":{\"absolutePath\":\"C:/Users/Adam/Documents/programation_projects/personal/Muberza/static/api/politicians/abir_moussi.json\"}}},{\"node\":{\"slug\":\"adbeldtif-al-oulwi\",\"facebookSlug\":\"ABDELLATIFALOUI22\",\"id\":\"dc62185a-54c4-5427-8bb6-24d860bbe359\",\"parent\":{\"absolutePath\":\"C:/Users/Adam/Documents/programation_projects/personal/Muberza/static/api/politicians/adbeldtif_al_oulwi.json\"}}},{\"node\":{\"slug\":\"mohamad-affes\",\"facebookSlug\":\"Dr.MohamedAffes\",\"id\":\"5ea7d794-c3b2-5093-bf78-cc18a6443624\",\"parent\":{\"absolutePath\":\"C:/Users/Adam/Documents/programation_projects/personal/Muberza/static/api/politicians/mohamad_affes.json\"}}},{\"node\":{\"slug\":\"ziad-hechmi\",\"facebookSlug\":\"elhechmi.ziad\",\"id\":\"29cf35d8-17c3-5b96-bb59-e63f9f61c7f6\",\"parent\":{\"absolutePath\":\"C:/Users/Adam/Documents/programation_projects/personal/Muberza/static/api/politicians/ziad_hechmi.json\"}}},{\"node\":{\"slug\":\"saif-din-makhlouf\",\"facebookSlug\":\"makseif.page\",\"id\":\"2d34a9da-23f1-5ec4-b9f4-ef0782c6bff5\",\"parent\":{\"absolutePath\":\"C:/Users/Adam/Documents/programation_projects/personal/Muberza/static/api/politicians/saif_makhlouf.json\"}}},{\"node\":{\"slug\":\"nedhal-soudi\",\"facebookSlug\":\"nidhalsaoudi89\",\"id\":\"5e6ad5b1-df72-5130-9f31-cfd492bb83eb\",\"parent\":{\"absolutePath\":\"C:/Users/Adam/Documents/programation_projects/personal/Muberza/static/api/politicians/nedhal_soudi.json\"}}}]")
+const mockData = JSON.parse("[{\"node\":{\"slug\":\"abir-moussi\",\"facebookSlug\":\"AbirMoussiOfficielle\",\"id\":\"58ce0cd4-65cf-5515-b0a8-83786a2a2ef7\",\"parent\":{\"absolutePath\":\"C:/Users/19083/OneDrive/Documents/programation_projects/personal/Muberza/static/api/politicians/abir_moussi.json\"}}},{\"node\":{\"slug\":\"adbeldtif-al-oulwi\",\"facebookSlug\":\"ABDELLATIFALOUI22\",\"id\":\"dc62185a-54c4-5427-8bb6-24d860bbe359\",\"parent\":{\"absolutePath\":\"C:/Users/19083/OneDrive/Documents/programation_projects/personal/Muberza/static/api/politicians/adbeldtif_al_oulwi.json\"}}},{\"node\":{\"slug\":\"mohamad-affes\",\"facebookSlug\":\"Dr.MohamedAffes\",\"id\":\"5ea7d794-c3b2-5093-bf78-cc18a6443624\",\"parent\":{\"absolutePath\":\"C:/Users/19083/OneDrive/Documents/programation_projects/personal/Muberza/static/api/politicians/mohamad_affes.json\"}}},{\"node\":{\"slug\":\"ziad-hechmi\",\"facebookSlug\":\"elhechmi.ziad\",\"id\":\"29cf35d8-17c3-5b96-bb59-e63f9f61c7f6\",\"parent\":{\"absolutePath\":\"C:/Users/19083/OneDrive/Documents/programation_projects/personal/Muberza/static/api/politicians/ziad_hechmi.json\"}}},{\"node\":{\"slug\":\"saif-din-makhlouf\",\"facebookSlug\":\"makseif.page\",\"id\":\"2d34a9da-23f1-5ec4-b9f4-ef0782c6bff5\",\"parent\":{\"absolutePath\":\"C:/Users/19083/OneDrive/Documents/programation_projects/personal/Muberza/static/api/politicians/saif_makhlouf.json\"}}},{\"node\":{\"slug\":\"nedhal-soudi\",\"facebookSlug\":\"nidhalsaoudi89\",\"id\":\"5e6ad5b1-df72-5130-9f31-cfd492bb83eb\",\"parent\":{\"absolutePath\":\"C:/Users/19083/OneDrive/Documents/programation_projects/personal/Muberza/static/api/politicians/nedhal_soudi.json\"}}}]")
 
+/**
+ * given a random unix epoch time, return a dayjs object with the first day of its month and year. 
+ */
+function normalizeMonth(timestamp){
+    let startRaw = new dayjs(timestamp * 1000)
+    let startYear = startRaw.year(); let startMonth = startRaw.month()
+    
+    return new dayjs(`${startMonth+1}/1/${startYear}/`)
+}
 
+function arrFullToCondensed(interval, dataArr){
+    
+    dataArr = dataArr.reverse();
 
-function arrFullToCondensed(start, interval, dataArr){
-    const resultArr = []    
+    let start = normalizeMonth(dataArr[0].timestamp)
     let end = start.add(interval.magnitude, interval.unit)
-    let condensedDataPoint = 0;
+    const resultArr = []; let condensedDataPoint = 0;
+    
 
     for(let i = 0; i<dataArr.length; i++){
 
-        let {views:dataPoint, timestamp:dataTimestamp} = dataArr[i]
-        dataTimestamp = new dayjs(dataTimestamp * 1000)
-        201019 >2020
-        //dataTimestamp < end
+        let dataPoint = dataArr[i].views;
+        let dataTimestamp = new dayjs(dataArr[i].timestamp * 1000)
+
         if(dataTimestamp.diff(end) <= 0){
             condensedDataPoint += dataPoint
-            continue
+            //if this is the last item and the condensedDataPoint is loaded, we don't want to waste it. we will push it even though we haven't surpassed the interval.
+            if(i != dataArr.length-1) continue
         }
         //dataTimestamp > end
         resultArr.push({"startTimestamp":start.unix(),"endTimestamp":end.unix(), "views":condensedDataPoint })
-        condensedDataPoint = dataTimestamp.diff(end) <= 0 ? dataPoint: 0;
         start = new dayjs(end);
         end = end.add(interval.magnitude, interval.unit)
+        
+        //triggers incase the dataTimestamp is beyond the end even after the interval increase. This means the interval had 0 views. 
+        if(dataTimestamp.diff(end) >=  0){
+            i--;
+            condensedDataPoint = 0
+            continue;
+        }
+        condensedDataPoint = dataPoint;
+        
     }
     return resultArr;
 }
@@ -51,21 +71,24 @@ async function main(query){
         })
         
         
-        
-        for(let i = 0; i< mockData.length; i++){ 
-            const {facebookSlug, parent:{absolutePath}} = mockData[i].node
+        //scrapes full viewership stats and adds them to the politician json file.
+        for(let i = 0; i< query.length; i++){ 
+            const {facebookSlug, parent:{absolutePath}} = query[i].node
 
             await page.goto(`https://www.facebook.com/${facebookSlug}/live_videos/`)
             await page.waitForSelector("._2pie");
 
-            const result = await page.evaluate(getStats)
+            const viewershipStatsObj = await page.evaluate(getStats)
+            const condensedArr =  arrFullToCondensed({'magnitude': 1, 'unit': "months"}, viewershipStatsObj.full)
+            
             
             let data = JSON.parse(fs.readFileSync(absolutePath));
-            data.viewershipStats = result;
+            
+            data.viewershipStats = viewershipStatsObj;
+            data.viewershipStats.condensed = condensedArr
+            
             fs.writeFileSync(absolutePath, JSON.stringify(data));
 
-            await page.waitForTimeout(parseInt(Math.random() * 50) * 100);
-            
         }
         
     }
@@ -83,9 +106,8 @@ async function main(query){
 for(let i = 0; i< mockData.length; i++){ 
     const {parent:{absolutePath}} = mockData[i].node
     
-    const fullArr = JSON.parse(fs.readFileSync(absolutePath)).viewershipStats.full.reverse();
-
-    const condensedArr =  arrFullToCondensed(new dayjs("2019"),{'magnitude': 4, 'unit': "weeks"}, fullArr )
+    const fullArr = JSON.parse(fs.readFileSync(absolutePath)).viewershipStats.full;
+    const condensedArr =  arrFullToCondensed({'magnitude': 1, 'unit': "months"}, fullArr )
     
     let data = JSON.parse(fs.readFileSync(absolutePath));
     data.viewershipStats.condensed = condensedArr;
